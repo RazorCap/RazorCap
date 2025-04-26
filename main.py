ea9608a84168b86d020b6bed4b215238591a3ecb
RazorCap <eddi18266@lj9n.underseagolf.com>
2025-04-25 20:38:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ session.headers = {
     'sec-fetch-dest': 'script',
     'sec-fetch-mode': 'no-cors',
     'sec-fetch-site': 'cross-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
 }
 
 params = {
